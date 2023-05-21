5d4f6ce77cc87be9430dc8343ad47f18630beae7
kevinzhuang79928 <103288943+Clatt79928@users.noreply.github.com>
2023-05-21 16:57:31 -0700

    need to finish implementing functions

---
+++ src/bot.py
@@ -0,0 +1,23 @@
+import requests 
+import json
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import os
+
+#store in .env file later
+#riot_token = "" 
+#discord_token = "" 
+
+#class TeemoBot:
+#def filter_spaces(nameWithSpaces):
+
+#def namesearch_engine(region,name):
+
+#async def riotna():
+
+
+
+
+    
+        
