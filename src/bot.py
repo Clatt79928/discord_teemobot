f7d04a93494ce58e57129726fa12f76dfb4c06a7
kevinzhuang79928 <103288943+Clatt79928@users.noreply.github.com>
2023-05-22 18:05:25 -0700

    retrieved summoner info

--- src/bot.py
+++ src/bot.py
@@ -1,24 +1,33 @@
 import requests 
 import json
 import discord
+import os
+from turtle import title
+from unittest import result
+from urllib import response
 from discord.ext import commands
 from discord.ext.commands import Bot
-import os
 from dotenv import load_dotenv
 
-#store in .env file later
-#riot_token = "" 
-#discord_token = "" 
+riot_token = load_dotenv("riot_key")
+discord_token = load_dotenv("discord_key") 
 
-#class TeemoBot:
-#def filter_spaces(nameWithSpaces):
+class TeemoBot:
+    def filter_spaces(nameWithSpaces):
+        result= " "
+        for i in nameWithSpaces:
+            result = result + " " + str(i)
+        return result
 
-#def namesearch_engine(region,name):
+    def namesearch_engine(region,name):
+        if region == "na":
+            riot_api = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + riot_token
+        response = requests.get(riot_api)
+        summononerDataJSON = response.json()
+        summonerID = summononerDataJSON['name']
+        summonerLvl = "Level: " + str(summononerDataJSON['summonerLevel'])
 
-#async def riotna():
-
-
-
-
-    
+        return (summonerID, summonerLvl)
         
+    async def riotna(command_message, *nameWithSpaces):
+    
