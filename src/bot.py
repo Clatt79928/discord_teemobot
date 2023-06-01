4b40e8cfcaa29c1bb337177c3b3c93433ebec3ef
kevinzhuang79928 <103288943+Clatt79928@users.noreply.github.com>
2023-06-01 15:53:19 -0700

    OMFG

--- src/bot.py
+++ src/bot.py
@@ -2,24 +2,25 @@ import requests
 import json
 import discord
 import os
-from turtle import title
 from unittest import result
 from urllib import response
 from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
-riot_token = load_dotenv("riot_key")
-discord_token = load_dotenv("discord_key") 
+load_dotenv()
+
+intents = discord.Intents.default()
+bot=commands.Bot(command_prefix='!', intents=intents)
+
+riot_token = os.getenv("riot_key")
+discord_token = os.getenv("discord_key") 
 
 class TeemoBot:
     def filter_spaces(nameWithSpaces):
         result = ' '.join(nameWithSpaces).replace(' ', '')
         return result
     
-
-
-    #sends a GET request to the Riot API to get the summoner's name and level
     def namesearch_engine(region,name):
         if region == "na":
             riot_api = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + riot_token
@@ -27,8 +28,21 @@ class TeemoBot:
         summononerDataJSON = response.json()
         summonerID = summononerDataJSON['name']
         summonerLvl = "Level: " + str(summononerDataJSON['summonerLevel'])
-        #create summonericon and return it
-
         return (summonerID, summonerLvl)  
         
-    #async def riotna(command_message, *nameWithSpaces):
+    @bot.command()
+
+    async def riotna(ctx, *nameWithSpaces):
+        name = TeemoBot.filter_spaces(nameWithSpaces)
+        summoner =TeemoBot.namesearch_engine("na", name)
+        embed =TeemoBot.discord.Embed(title=summoner[0], description=summoner[1], color=0xFFD500)
+        #embed.set_thumbnail(url=summoner[2])
+        await ctx.send(embed=embed)
+
+    bot= discord.Client(intents=discord.Intents.default())
+
+    @bot.event
+    async def on_ready():
+        print("frick u")
+        
+    bot.run(discord_token)
